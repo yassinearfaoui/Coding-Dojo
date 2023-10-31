25009d14c5531050ca3020456a9de1a4736a7f13
yassinearfaoui <yassinearf123@gmail.com>
2023-10-31 21:58:27 +0100

    commit

---
+++ WebFundamentals/Week 1/Day 5/Practice/loop-challenges.js
@@ -0,0 +1,21 @@
+for (var i=1 ;i <=20 ; i++)
+{
+    if (i%2==1) console.log(i);
+}
+
+// decreasing multiples of 3 
+for (var i=100 ; i>=0 ;i--)
+{
+    if (i%3==0) console.log(i);
+} 
+
+// print the sequence 
+for(var i=4 ; i>=-3.5; i-=1.5)console.log(i); 
+
+// sigma 
+var sigma=0 ;
+for  (var i=1 ; i<=100;i++) sigma +=i ;
+console.log(sigma); 
+
+// factorial 
+fact = 1;
---
+++ WebFundamentals/Week 2/Day 1/Practice/video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+function playvideo(element){
+    element.play();
+}
+function pausevideo(element){
+    element.pause()
+}
+function muted(){
+   element.muted = true;
+}
+
