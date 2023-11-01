bb530a148921660806b09a605c916b195e3eafa8
yassinearfaoui <yassinearf123@gmail.com>
2023-11-01 15:10:57 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Core/like2/like2.js
@@ -0,0 +1,10 @@
+function count0() {
+    document.querySelector("#sp0").innerHTML++;
+}
+
+function count1() {
+    document.querySelector("#sp1").innerHTML++;
+}
+    function count2() {
+        document.querySelector("#sp2").innerHTML++;
+    }
---
+++ WebFundamentals/Week 2/Day 1/Core/likes/likes.js
@@ -0,0 +1,3 @@
+function likes(){
+    document.querySelector("#numlike").innerHTML++;
+}
