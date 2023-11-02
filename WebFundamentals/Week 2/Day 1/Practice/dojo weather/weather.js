740d7254d77c4e2c85a7a265f2d1c9cf47136c16
yassinearfaoui <yassinearf123@gmail.com>
2023-11-02 12:04:20 +0100

    commit

---
+++ WebFundamentals/Week 2/Day 1/Practice/dojo weather/weather.js
@@ -0,0 +1,11 @@
+function alertme(element){
+    alert();
+}
+function remove(){
+var element = document.querySelector(".buttom");
+element.remove();
+}
+
+function changetemp(){
+document.querySelector(".tempc").innerHTML*33.8;
+    }
