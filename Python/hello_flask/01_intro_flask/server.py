c5b5cc53781ac909161003037f4ae8b7efda4087
yassinearfaoui <yassinearf123@gmail.com>
2023-11-17 09:10:35 +0100

    commit

--- Python/hello_flask/01_intro_flask/server.py
+++ Python/hello_flask/01_intro_flask/server.py
@@ -1,8 +1,49 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask , render_template 
+app = Flask(__name__)    
 
+# http://127.0.0.1/
+@app.route('/')
+def hello_world():
+    return 'Hello World!' 
+
+# http://127.0.0.1/hello
+@app.route('/hello')
+def hello():
+    return "Hello From Server🚀🚀🚀"
+
+# http://127.0.0.1/hello/user
+@app.route('/hello/user')
+def say_hello():
+    return "<h1>Hello James 😁😁😁</h1>"
+
+# http://127.0.0.1/hello/user/<username>
+@app.route('/hello/user/<username>')
+def say_hello_username(username):
+    return f"<h1>Hello {username} 😁😁😁</h1>"
+
+# http://127.0.0.1/hello/user/<username>/<times>
+@app.route('/hello/user/<username>/<int:times>')
+def say_hello_username_n_times(username, times):
+    return f"<h1>Hello {username} 😁😁😁</h1>" * times
+
+# http://127.0.0.1/user/<username>/<age>
+@app.route('/user/<username>/<int:age>')
+def user_info(username, age):
+    return f"<h1>USERNAME : {username} <br/> AGE : {age} </h1>"
+
+# http://127.0.0.1/index/john/54
+@app.route('/index/<username>/<int:age>')
+def index(username, age):
+    users = [
+        {'name':"John", 'age':35},
+        {'name':"Sarah", 'age':25},
+        {'name':"Alex", 'age':28},
+        {'name':"Amelia", 'age':23},
+        {'name':"James", 'age':22},
+        {'name':"Eric", 'age':56}]
+    return render_template("index.html", user = username, number = age, users=users)
+
+
+
+if __name__=="__main__":   
+    app.run(debug=True, port=1337) 
