59ba37ae2a698a9e7f62efe9fc58df6d24909734
yassinearfaoui <yassinearf123@gmail.com>
2023-11-17 12:57:27 +0100

    commit

---
+++ Python/Week 1/Day 3/Practice/Basketball Dictionaries.py
@@ -0,0 +1,77 @@
+players = [
+    {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Jason Tatum",  
+    	"age":24, 
+    	"position": "small forward", 
+    	"team": "Boston Celtics"
+    },
+    {
+    	"name": "Kyrie Irving",         
+    	"age":32,
+        "position": "Point Guard", 
+    	"team": "Brooklyn Nets"
+    },
+    {
+    	"name": "Damian Lillard", 
+    	"age":33,
+        "position": "Point Guard", 
+    	"team": "Portland Trailblazers"
+    },
+    {
+    	"name": "Joel Embiid", 
+    	"age":32,
+        "position": "Power Foward", 
+    	"team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+class Player:
+    def __init__(self, yousef):
+        self.name = yousef['name']
+        self.age = yousef['age']
+        self.position = yousef['position']
+        self.team = yousef['team']
+
+    
+
+    @classmethod
+    def get_team(cls, team_list):
+        empty_list=[]
+        for x in team_list:
+            empty_list.append(cls(x))
+        return empty_list
+
+    def display(self):
+        print(self.name ,self.age)
+
+
+kevin=Player( {
+    	"name": "Kevin Durant", 
+    	"age":34, 
+    	"position": "small forward", 
+    	"team": "Brooklyn Nets"
+    })
+kevin.display()
+
+
+
+# jason=Player("Jason Tatum","24","small forward","Boston Celtics")
+# print(jason)
+
+# kyrie=Player("Kyrie Irving","32","Point Guard","Brooklyn Nets")
+# print(kyrie)
+    
+
+# print()   
