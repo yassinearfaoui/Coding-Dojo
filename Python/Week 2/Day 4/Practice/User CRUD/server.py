c64da06e3fbee1bb586f7e1edc30ad8fe81dc01e
yassinearfaoui <yassinearf123@gmail.com>
2023-11-26 21:11:14 +0100

    commit

---
+++ Python/Week 2/Day 4/Practice/User CRUD/server.py
@@ -0,0 +1,61 @@
+from flask import Flask,render_template,redirect,request
+from user_model import User
+app = Flask(__name__)
+
+@app.route('/')
+def AllUsers():
+    users=User.get_all()
+    return render_template("users.html", users=users)    
+
+
+
+@app.route('/new user')
+def insert():
+    return render_template("newuser.html")
+
+
+@app.route("/user/create", methods=["post"])
+def create_user():
+    data = {
+        "First_name": request.form["First_name"],
+        "Last_name": request.form["Last_name"],
+        "Email": request.form["Email"],
+    }
+    User.create(data)
+    return redirect("/")
+
+@app.route("/user/<int:id>")
+def one_user(id):
+    one_user= User.get_one_by_id({"id": id})
+    return render_template("one_user.html", user=one_user)
+
+
+@app.route("/user/<int:id>/edit")
+def edit(id):
+    edit_user = User.get_one_by_id({'id':id})
+    return render_template("EditUser.html" , User = edit_user)
+
+@app.route("/user/update" , methods=["post"])
+def update():
+    data = {
+        "id": request.form["id"],
+        "First_name": request.form["First_name"],
+        "Last_name": request.form["Last_name"],
+        "Email": request.form["Email"],
+    }
+    print(data)
+    User.update(data)
+    return redirect("/")
+
+@app.route("/user/<int:id>/destroy")
+def destroy(id):
+    User.delete({"id": id})
+    return redirect("/")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True , port=5001)
