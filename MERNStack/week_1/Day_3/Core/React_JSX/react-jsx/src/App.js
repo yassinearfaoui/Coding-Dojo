ca378bb2542c6d31ab796ecf3540821b66e54f6a
yassinearfaoui <yassinearf123@gmail.com>
2023-12-21 13:26:56 +0100

    commit

---
+++ MERNStack/week_1/Day_3/Core/React_JSX/react-jsx/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>
+      Hello Dojo
+    </h1>
+    <h3>
+      Things I need to do
+    </h3>
+    <ol>
+      <li>Learn React</li>
+      <li>Climb Mt.Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the Dogs</li>
+    </ol>
+    </div>
+  );
+}
+
+export default App;
