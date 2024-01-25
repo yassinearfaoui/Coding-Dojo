1acbd6a29fd74b49e5fc55c82ca43b2d39ea384b
yassinearfaoui <yassinearf123@gmail.com>
2024-01-25 12:54:43 +0100

    commit

---
+++ CSharpStack/week_1/Day_2/Practice/Ride Mader/IUseWeapon.cs
@@ -0,0 +1,7 @@
+interface IUseWeapon
+{
+    string Weapon {get;set;}
+    string Type {get;set;}
+
+    void UseWeapon();
+}
---
+++ CSharpStack/week_1/Day_2/Practice/Ride Mader/Program.cs
@@ -0,0 +1,10 @@
+﻿public class  Vehicule : IUseWeapon
+{
+    public string Name {get;set ;}
+    public int  Number {get;set ;}
+     public string Color {get;set ;}
+     public bool Engine {get ; set;}
+
+
+
+}
