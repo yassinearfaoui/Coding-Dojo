f1598cd929ed516df0c31a8ec48366d5f07ee6d9
yassinearfaoui <yassinearf123@gmail.com>
2024-01-31 10:11:33 +0100

    FuelUp

---
+++ CSharpStack/week_1/Day_2/Practice/Fuel Up/Bicycle.cs
@@ -0,0 +1,4 @@
+public class Bicycle : Vehicle
+{
+    public Bicycle(string n, string c, int ts) : base(n, 1, c, false, ts) {}
+}
---
+++ CSharpStack/week_1/Day_2/Practice/Fuel Up/Program.cs
@@ -0,0 +1,30 @@
+﻿// Create 4 vehicles using each constructor at least once
+Car car = new Car("Nissan Frontier", 5, "Red", 120, "Gas");
+Bicycle bicycle = new Bicycle("Schwinn", "Silver", 30);
+Horse horse = new Horse("Donna", "Brown", 20, "Hay");
+// This causes a problem because Vehicle is abstract now
+// Vehicle scooter = new Vehicle("Scooter", "Black");
+
+// Put all the vehicles created into a List
+List<Vehicle> AllVehicles = new List<Vehicle>();
+AllVehicles.Add(car);
+AllVehicles.Add(bicycle);
+AllVehicles.Add(horse);
+
+// Create a List of INeedFuel, but do not add anything to it yet
+List<INeedFuel> NeedsFuel = new List<INeedFuel>();
+
+// Loop through the List of Vehicles and if an item has the INeedFuel interface applies, add it to the INeedFuel List
+foreach(Vehicle v in AllVehicles)
+{
+    if(v is INeedFuel)
+    {
+        NeedsFuel.Add((INeedFuel)v);
+    }
+}
+
+// Take your finished INeedFuel List, loop through it, and give each item 10 units of fuel
+foreach(INeedFuel i in NeedsFuel)
+{
+    i.GiveFuel(10);
+}
